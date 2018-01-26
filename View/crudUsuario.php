a746a8fa4e8550b0537c0aaf88da4d2f4fecfef9
Bruno Haick <brunohaick@gmail.com>
2018-01-26 13:10:36 -0300

    adicionando estilos e tabela

--- View/crudUsuario.php
+++ View/crudUsuario.php
@@ -1,82 +1,115 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8" >
-        <title>FEPESPA</title>
-        <link type="text/css" rel="stylesheet" href="View/css/bootstrap.css"/>
+<?php
+	include('./Model/usuarioModel.php');
+	$usuariosModel = new usuarioModel();
+	$usuarios = $usuariosModel->getAllUsersPag();
+	echo '<h1>Users CRUD - Usuarios Confirmados</h1>';
+	if (sizeof($usuarios) >= 1) {
+			$html = "
+			<table class = \"table table-striped\">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Nome</th>
+						<th>Email</th>
+						<th>Cart.</th>
+						<th>Pagamentos</th>
+						<th>Cat.</th>						
+					</tr>
+				</thead>
+				<tbody>';
+		foreach ($usuarios as &$u) {
+			$html .= "
+			<table class = \"table table-striped\">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Nome</th>
+						<th>Email</th>
+						<th>Cart.</th>
+						<th>Pagamentos</th>
+						<th>Cat.</th>						
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope =\"row\">1</th>
+						<td>Mark</td>
+						<td>Otto</td>
+						<td>@mdo</td>
+						</tr>
+					<tr>
+						<th scope = \"row\">2</th>
+						<td>Jacob</td>
+						<td>Thornton</td>
+						<td>@fat</td>
+					</tr>
+					<tr>
+						<th scope = \"row\">3</th>
+						<td>Larry</td>
+						<td>the Bird</td>
+						<td>@twitter</td>
+					</tr>
+				</tbody>
+		</table>
+	';
 
-        <script src="View/js/bootstrap.js"> </script>
-        <script src="View/js/jquery.js"> </script>
-        
-    </head>
-    <body>
-        <?php
-        include('./Model/usuarioModel.php');
-        $usuariosModel = new usuarioModel();
-        $usuarios = $usuariosModel->getAllUsersPag();
-        echo '<h1>Users CRUD - Usuarios Confirmados</h1>';
-        if (sizeof($usuarios) >= 1) {
-            echo "<table>";
-            foreach ($usuarios as &$u) {
-                echo '<tr>';
-                echo '<td>';
-                echo $u['display_name'];
-                echo '</td>';
-                echo '<td>';
-                echo $u['user_login'];
-                echo '</td>';
-                echo '<td>';
-                echo $u['data_filiacao'];
-                echo '</td>';
-                echo '<td>';
-                echo $u['user_email'];
-                echo '</td>';
-                echo '<td>';
-                echo $u['carteirinha'];
-                echo '</td>';
-                echo '<td>';
-                echo '<a type="button" href="/carterinha/'.$u['ID'].'/0"> Gerar Carteirinha</a>';
-                echo '<a type="button" href="/pagamento/'.$u['ID'].'/0"> Pagamentos</a>';
-                echo '</td>';
-                echo '</tr>';
-            }
-            echo '</table>';
-        } else {
-            echo '<b>Sem Usuarios</b>';
-            echo '</br>';
-        }
+		echo '<tr>';
+			echo '<td>';
+			echo $u['display_name'];
+			echo '</td>';
+			echo '<td>';
+			echo $u['user_login'];
+			echo '</td>';
+			echo '<td>';
+			echo $u['data_filiacao'];
+			echo '</td>';
+			echo '<td>';
+			echo $u['user_email'];
+			echo '</td>';
+			echo '<td>';
+			echo $u['carteirinha'];
+			echo '</td>';
+			echo '<td>';
+			echo '<a type="button" href="/carterinha/'.$u['ID'].'/0"> Gerar Carteirinha</a>';
+			echo '<a type="button" href="/pagamento/'.$u['ID'].'/0"> Pagamentos</a>';
+			echo '</td>';
+			echo '</tr>';
+		}
+		echo '</table>';
+	} else {
+		
+		
+		
+		echo '<b>Sem Usuarios</b>';
+		echo '</br>';
+	}
 
-        $usuarios = $usuariosModel->getAllUsersNOTPag();
-        echo '<h1>Users CRUD - Usuarios Pendentes</h1>';
-        echo "<table style='border:1px'>";
-        foreach ($usuarios as &$u) {
-            echo '<tr>';
-            echo '<td>';
-            echo $u['display_name'];
-            echo '</td>';
-            echo '<td>';
-            echo $u['user_login'];
-            echo '</td>';
-            echo '<td>';
-            echo $u['data_filiacao'];
-            echo '</td>';
-            echo '<td>';
-            echo $u['user_email'];
-            echo '</td>';
-            echo '<td>';
-            echo $u['carteirinha'];
-            echo '</td>';
-            echo '<td>';
-           echo '<a type="button" href="/carterinha/'.$u['ID'].'/1"> Confirmar Pagamento</a>';
-            echo '</td>';
-            echo '</tr>';
-        }
-        echo '</table>'
-        ?>
-    </body>
+	$usuarios = $usuariosModel->getAllUsersNOTPag();
+	echo '<h1>Users CRUD - Usuarios Pendentes</h1>';
+	echo "<table style='border:1px'>";
+	foreach ($usuarios as &$u) {
+		echo '<tr>';
+		echo '<td>';
+		echo $u['display_name'];
+		echo '</td>';
+		echo '<td>';
+		echo $u['user_login'];
+		echo '</td>';
+		echo '<td>';
+		echo $u['data_filiacao'];
+		echo '</td>';
+		echo '<td>';
+		echo $u['user_email'];
+		echo '</td>';
+		echo '<td>';
+		echo $u['carteirinha'];
+		echo '</td>';
+		echo '<td>';
+	   echo '<a type="button" href="/carterinha/'.$u['ID'].'/1"> Confirmar Pagamento</a>';
+		echo '</td>';
+		echo '</tr>';
+	}
+	echo '</table>'
+?>
+</body>
 </html>
---
+++ View/template/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="pt-BR">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!--<link rel="shortcut icon" href="template/img/favicon.png" type="image/x-icon" />-->
+        <title>FEPESPA</title>
+
+        <link type="text/css" rel="stylesheet" href="template/css/bootstrap.css" />
+
+        <script src="template/js/jquery.js"></script>
+        <script src="template/js/bootstrap.js"></script>
+    </head>
+
+    <body>
